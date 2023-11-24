5a77adffd611fc6b9633f4fec1d27705c639aa66
Trevor Augustine <trevor.okinda@strathmore.edu>
2023-11-25 01:36:56 +0300

    loaded dataset

---
+++ Consolidation.R
@@ -0,0 +1,39 @@
+#134780 4.2 C
+# Load dataset
+WeatherData <- read.csv("data/Weather.csv", colClasses = c(
+  MinTemp = "numeric",
+  MaxTemp = "numeric",
+  Rainfall = "numeric",
+  Evaporation = "numeric",
+  Sunshine = "numeric",
+  WindGustDir = "factor",
+  WindGustSpeed = "numeric",
+  WindDir9am = "factor",
+  WindDir3pm = "factor",
+  WindSpeed9am = "numeric",
+  WindSpeed3pm = "numeric",
+  Humidity9am = "numeric",
+  Humidity3pm = "numeric",
+  Pressure9am = "numeric",
+  Pressure3pm = "numeric",
+  Cloud9am = "numeric",
+  Cloud3pm = "numeric",
+  Temp9am = "numeric",
+  Temp3pm = "numeric",
+  RainToday = "factor",
+  RISK_MM = "numeric",
+  RainTomorrow = "factor"
+))
+
+# Define levels for categorical columns
+wind_dir_levels <- c("N", "NE", "E", "SE", "S", "SW", "W", "NW")
+rain_levels <- c("No", "Yes")
+
+# Update factor columns with levels
+WeatherData$WindGustDir <- factor(WeatherData$WindGustDir, levels = wind_dir_levels)
+WeatherData$WindDir9am <- factor(WeatherData$WindDir9am, levels = wind_dir_levels)
+WeatherData$WindDir3pm <- factor(WeatherData$WindDir3pm, levels = wind_dir_levels)
+WeatherData$RainToday <- factor(WeatherData$RainToday, levels = rain_levels)
+WeatherData$RainTomorrow <- factor(WeatherData$RainTomorrow, levels = rain_levels)
+
+View(WeatherData)

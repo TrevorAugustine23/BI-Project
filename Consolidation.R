a53de14084c82b7c4a1abede1a6d10654038bb6a
Trevor Augustine <trevor.okinda@strathmore.edu>
2023-11-25 01:44:18 +0300

    Extract JSON data from the request

--- Consolidation.R
+++ Consolidation.R
@@ -1,3 +1,10 @@
 #134780 4.2 C
 #Install packages
 install.packages("plumber")
+
+# Load necessary libraries and the model
+library(plumber)
+library(caret) 
+
+predict_model <- function(req) {
+  input_data <- req$postBody  # Extract JSON data from the request

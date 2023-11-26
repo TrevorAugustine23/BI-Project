cc8bae197e5723cf87f846aa74b3cab107072fbd
Trevor Augustine <trevor.okinda@strathmore.edu>
2023-11-26 21:46:41 +0300

    saving the model

--- Consolidation.R
+++ Consolidation.R
@@ -1,27 +1,2 @@
 #134780 4.2 C
-#Install packages
-install.packages("plumber")
-
-# Load necessary libraries and the model
-library(plumber)
-library(caret) 
-
-predict_model <- function(req) {
-  input_data <- req$postBody  # Extract JSON data from the request
-  
-  # Convert JSON to a list or data frame for prediction
-  input_features <- fromJSON(input_data)
-  
-  # Make predictions using the trained model
-  predictions <- predict(rf_model, input_features)
-  
-  # Convert predictions to a JSON response
-  response <- list(predictions = predictions)
-  return(response)
-}
-
-# Create a Plumber router
-api <- plumb("Consolidation.R")  # Replace "your_api.R" with your script filename
-
-# Define the endpoint and link it to the prediction function
-api$run(port = 8000)  # Set the port number as needed
+#Saving the model

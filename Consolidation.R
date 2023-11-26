5675ac962de5c79725b1cd94fc5b195704945899
Trevor Augustine <trevor.okinda@strathmore.edu>
2023-11-26 22:13:16 +0300

    API details

--- Consolidation.R
+++ Consolidation.R
@@ -43,3 +43,30 @@ print(predictions_loaded_model)
 # Load the saved RandomForest model
 loaded_rf_model <- readRDS("./models/saved_rf_model.rds")
 
+#* @apiTitle Rain Prediction Model API
+
+#* @apiDescription Used to predict whether it will rain tomorrow.
+
+#* @param MaxTemp Maximum temperature
+#* @param MinTemp Minimum temperature
+#* @param Rainfall Amount of rainfall
+#* @param Evaporation Evaporation
+#* @param Sunshine Sunshine hours
+#* @param WindGustDir Wind direction at gust time
+#* @param WindGustSpeed Wind speed (gust time)
+#* @param WindDir9am Wind direction at 9 am
+#* @param WindDir3pm Wind direction at 3 pm
+#* @param WindSpeed9am Wind speed (9 am)
+#* @param WindSpeed3pm Wind speed (3 pm)
+#* @param Humidity9am Humidity at 9 am
+#* @param Humidity3pm Humidity at 3 pm
+#* @param Pressure9am Atmospheric pressure at 9 am
+#* @param Pressure3pm Atmospheric pressure at 3 pm
+#* @param Cloud9am Cloud cover at 9 am
+#* @param Cloud3pm Cloud cover at 3 pm
+#* @param Temp9am Temperature at 9 am
+#* @param Temp3pm Temperature at 3 pm
+#* @param RainToday Whether it will rain today (Yes/No)
+#* @param RISK_MM Risk of rain in mm
+#* @param RainTomorrow Actual observation: Will it rain tomorrow? (Yes/No)
+
